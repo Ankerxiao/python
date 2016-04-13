8d0e8220829b4ae0d7d292fe925815f46bea4247
xiao <807132689@qq.com>
2016-04-13 10:17:07 +0800

    已经带正文，但正文乱码，暂不解决

--- email/attach_sendmail.py
+++ email/attach_sendmail.py
@@ -4,10 +4,15 @@
 import os
 import smtplib
 import time
+import re
 
 from email.mime.text import MIMEText
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 #邮箱基本信息
 smtpserver = 'smtp.mxhichina.com'
@@ -16,16 +21,16 @@ password= 'q1w2E#R$'
 from_addr = username
 to_addr = 'huang.xiaokun@synitalent.com'
 
-msg = MIMEMultipart('related')
-msg['Subject'] = '附件测试邮件'
+msg = MIMEMultipart()
+msg['Subject'] = u"附件测试邮件,heheda"
 msg['From'] = from_addr
 msg['To'] = to_addr
 
-message = "hello,有邮件正文了"
-body = MIMEText('hello,有邮件正文了','text','utf-8')
+mail_body = "hello,拜托啊，有邮件正文了,中文已经不乱码喽"
+body = MIMEText(mail_body,"text","utf-8")
 msg.attach(body)
 
-att = MIMEText(open('text_sendmail.py','rb').read(),'base64','utf-8')
+att = MIMEText(open('html_sendmail.py','rb').read(),'base64','utf-8')
 att['Content-Type'] = 'application/octet-stream'
 att['Content-Disposition'] = 'attachment;filename="text_sendmail.py"'
 msg.attach(att)
